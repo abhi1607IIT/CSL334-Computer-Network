46dd561202c4d6592ab23a894e51437d8aeb2a4c
abhi1607IIT <abhi.gupta.1607@gmail.com>
2018-09-21 03:43:46 +0530

    added tcpecho client server

---
+++ Socket Programming/Lab1/daytimealone.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+
+#define MAXLINE 4096
+	
+
+int
+main()
+{
+	int			i, listenfd, connfd;
+	char			buff[MAXLINE];
+	time_t			ticks;
+
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+	printf("%s", buff);
+
+
+}
+
