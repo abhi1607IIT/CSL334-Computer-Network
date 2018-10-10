cc56ea6bc264959b54ee0fd2d3654b1313baef50
abhi1607IIT <abhi.gupta.1607@gmail.com>
2018-10-10 09:51:53 +0530

    added q5 for assign 5

--- Socket Programming/Assign5/Q5/q5.c
+++ Socket Programming/Assign5/Q5/q5.c
@@ -11,26 +11,63 @@
 
 int main(int argc, char *argv[])
 {
-    struct hostent *lh = gethostbyname(argv[1]);
+    printf("Press 1 for gethostbyname and 2 for gethostbyaddr \n");
+    int d;
+    scanf("%d",&d);
+    if(d==1){
+        int argument;
+        printf("Enter the number of the hostname to be considered \n");
+        scanf("%d",&argument);
+        struct hostent *lh = gethostbyname(argv[argument]);
 
-    if (lh){
-    	printf("%s\n",lh->h_name);
-    	printf("%d\n",lh->h_addrtype);
-    	printf("%d\n",lh->h_length);
-    	int i=0;
-    	while((lh->h_aliases)[i]){
-    		printf("%s\n",(lh->h_aliases)[i]);
-    		i++;
-    	}
-    	i=0;
-    	while((lh->h_addr_list)[i]){
-    		printf("%s\n",(lh->h_addr_list)[i]);
-    		i++;
-    	}
+        if (lh){
+            printf("%s\n",lh->h_name);
+            printf("%d\n",lh->h_addrtype);
+            printf("%d\n",lh->h_length);
+            int i=0;
+            while((lh->h_aliases)[i]){
+                printf("%s\n",(lh->h_aliases)[i]);
+                i++;
+            }
+            i=0;
+            while((lh->h_addr_list)[i]){
+                printf("%s\n",(lh->h_addr_list)[i]);
+                i++;
+            }
+        }
+        else{
+            herror("gethostbyname");
+        }
+    }
+    else if (d==2){
+        int argument;
+        printf("Enter the number of the ip address to be considered \n");
+        scanf("%d",&argument);
+        struct in_addr addr;
+        inet_pton(AF_INET,argv[argument],&addr);
+        struct hostent *rh = gethostbyaddr(&addr,sizeof addr,AF_INET);
+        if (rh){
+            printf("%s\n",rh->h_name);
+            printf("%d\n",rh->h_addrtype);
+            printf("%d\n",rh->h_length);
+            int i=0;
+            while((rh->h_aliases)[i]){
+                printf("%s\n",(rh->h_aliases)[i]);
+                i++;
+            }
+            i=0;
+            while((rh->h_addr_list)[i]){
+                printf("%s\n",(rh->h_addr_list)[i]);
+                i++;
+            }
+        }
+        else{
+            herror("gethostbyname");
+        }
     }
     else{
-        herror("gethostbyname");
+        printf("Enter proper choice\n");
     }
-
+        
     return 0;
 }
